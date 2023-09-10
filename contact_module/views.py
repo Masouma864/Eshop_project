28e2b7562a673d3cb02753f2575ee59787838b83
Masouma <masouma2020@gmail.com>
2023-09-11 00:27:02 +0430

    Add new functionality like uploading , using session

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render, redirect
-from django.views import View
+from django.views.generic import ListView
 
-from .forms import ContactUsForm, ContactUsModelForm
-# from .models import ContactUs
+from .forms import ContactUsModelForm
+from django.views.generic.edit import CreateView
 
-# Create your views here.
-from django.urls import reverse
+from .models import UserProfile
 
 
-class ContactUsView(View):
-    def get(self, request):
-        contact_form = ContactUsModelForm()
-        return render(request, 'contact_module/contact_us_page.html', {
-            'contact_form': contact_form
-        })
+class ContactUsView(CreateView):
+    form_class = ContactUsModelForm
+    template_name = 'contact_module/contact_us_page.html'
+    success_url = '/contact-us/'
 
-    def post(self, request):
-        contact_form = ContactUsModelForm(request.POST)
-        if contact_form.is_valid():
-            contact_form.save()
-            return redirect('home_page')
 
-        return render(request, 'contact_module/contact_us_page.html', {
-            'contact_form': contact_form
-        })
+def store_file(file):
+    with open('temp/image.jpg', "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
+class CreateProfileView(CreateView):
+    template_name = 'contact_module/create_profile_page.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/contact-us/create-profile'
+
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = 'contact_module/profiles_list_page.html'
+    context_object_name = 'profiles'
--- product_module/views.py
+++ product_module/views.py
@@ -1,23 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.base import TemplateView, View
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 
-# Create your views here.
+class ProductListView(ListView):
+    template_name = 'product_module/product_list.html'
+    model = Product
+    context_object_name = 'products'
 
 
-def product_list(request):
-    products = Product.objects.all().order_by('-price')[:4]
-    return render(request, 'product_module/product_list.html', {
-        'products': products,
-    })
+class ProductDetailView(DetailView):
+    template_name = 'product_module/product_detail.html'
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_product = self.object
+        request = self.request
+        favorite_product_id = request.session.get("product_favorites")
+        context['is_favorite'] = favorite_product_id == str(loaded_product.id)
+        return context
 
 
-def product_detail(request, slug):
-    # try:
-    #     product = Product.objects.get(id=product_id)
-    # except:
-    #     raise Http404()
-    product = get_object_or_404(Product, slug=slug)
-    return render(request, 'product_module/product_detail.html', {
-        'product': product
-    })
+class AddProductFavorite(View):
+    def post(self, request):
+        product_id = request.POST["product_id"]
+        product = Product.objects.get(pk=product_id)
+        request.session["product_favorites"] = product_id
+        return redirect(product.get_absolute_url())
