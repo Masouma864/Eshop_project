816aa083cef9263adf2d9696e0aa1bacaf064937
Masouma <masouma2020@gmail.com>
2023-09-13 06:13:52 +0430

    Make header dynamic

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home_module',
     'product_module',
     'contact_module',
+    'site_module',
     # external apps
     'django_render_partial',
 ]
@@ -139,6 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'onionarchitecturemvc@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangotoplearn@pass'
 EMAIL_PORT = 587
--- home_module/views.py
+++ home_module/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.views import View
 from django.views.generic.base import TemplateView
 
-
-# class HomeView(View):
-#     def get(self, request):
-#         context = {
-#             'data': 'this is data'
-#         }
-#         return render(request, 'home_module/index_page.html', context)
+from site_module.models import SiteSetting, FooterLinkBox
 
 
 class HomeView(TemplateView):
@@ -24,11 +16,30 @@ class HomeView(TemplateView):
 
 
 def site_header_component(request):
+    setting: SiteSetting = SiteSetting.objects.filter(is_main_setting=True).first()
     context = {
-        'link': 'آموزش جنگو'
+        'site_setting': setting
     }
     return render(request, 'shared/site_header_component.html', context)
 
 
 def site_footer_component(request):
-    return render(request, 'shared/site_footer_component.html', {})
+    setting: SiteSetting = SiteSetting.objects.filter(is_main_setting=True).first()
+    footer_link_boxes = FooterLinkBox.objects.all()
+    for item in footer_link_boxes:
+        item.footerlink_set
+    context = {
+        'site_setting': setting,
+        'footer_link_boxes': footer_link_boxes
+    }
+    return render(request, 'shared/site_footer_component.html', context)
+
+
+class AboutView(TemplateView):
+    template_name = 'home_module/about_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        site_setting: SiteSetting = SiteSetting.objects.filter(is_main_setting=True).first()
+        context['site_setting'] = site_setting
+        return context
