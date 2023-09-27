9e2d42dc3ff8f0f2ef89f4c4c926d1d68d6ed389
Masouma <masouma2020@gmail.com>
2023-09-27 05:19:50 +0430

    Show the product of each category

--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -13,3 +13,8 @@ def cut(value, arg):
 @register.filter(name='show_jalali_date')
 def show_jalali_date(value):
     return date2jalali(value)
+
+
+@register.filter(name='three_digits_currency')
+def three_digits_currency(value: int):
+    return '{:,}'.format(value) + ' تومان'
